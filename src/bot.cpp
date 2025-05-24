74d8cb784bd084688a0b3c68c70af2c7d6546855
Max <maximilianostwald64@gmail.com>
2025-05-24 13:22:00 +0200

    Made a bot class

---
+++ src/bot.cpp
@@ -0,0 +1,21 @@
+#include "bot.hpp"
+
+bot::bot(const std::string& bot_token) : m_BOT_TOKEN(bot_token), m_BOT(bot_token) {}
+
+int bot::initBot() {
+    m_BOT.on_log(dpp::utility::cout_logger());
+
+    m_BOT.on_slashcommand([this](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+
+    m_BOT.on_ready([this](const dpp::ready_t& event) {
+        dpp::slashcommand ping_cmd("ping", "Pong!", m_BOT.me.id);
+        m_BOT.global_command_create(ping_cmd);
+    });
+
+    m_BOT.start(dpp::st_wait);
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
-#include <dpp/dpp.h>
-
-const std::string BOT_TOKEN = "add your token here";
+#include "bot.hpp"
 
 int main() {
-    dpp::cluster bot(BOT_TOKEN);
+    bot myBot("Test");
 
-    bot.on_log(dpp::utility::cout_logger());
+    myBot.initBot();
 
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        }
-    });
-
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
-        }
-    });
-
-    bot.start(dpp::st_wait);
+    return EXIT_SUCCESS;
 }
